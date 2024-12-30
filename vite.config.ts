04a09e14589f685a6af1a6d64acd0626b21d2f65
guo764672608 <guo764672608>
2024-12-30 20:25:34 +0800

    feat: 修改打包方式

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist', // 自定义构建输出目录
     target: 'es2020',
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
-      // name: 'snail-components',
       formats: ['cjs', 'es'],
-      // fileName: format => `index.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
